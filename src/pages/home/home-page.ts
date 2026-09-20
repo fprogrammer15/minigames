f840b253630094ce118a0ab0e03f6b51a10ef7af
dev <haydaraliyevasevdo26@gmail.com>
2026-09-20 17:39:59 +0500

    feat: compose home page from sections

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,9 +1,16 @@
+import { developerSectionHtml } from '../../features/developer-section/developer-section';
+import { heroHtml } from '../../features/hero/hero';
+import { leaderboardHtml } from '../../features/leaderboard/leaderboard';
+import { sliderHtml } from '../../features/slider/slider';
 import { html } from '../../utils/dom';
 
 export function renderHomePage(): HTMLElement {
   return html(`
     <div class="home">
-      <h1 class="home__title">MiniGames</h1>
+      ${heroHtml()}
+      ${sliderHtml()}
+      ${leaderboardHtml()}
+      ${developerSectionHtml()}
     </div>
   `);
 }
