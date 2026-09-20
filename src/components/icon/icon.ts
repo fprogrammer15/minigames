4b9a49b99de2610226cb3df78ddc890c95bd040c
dev <haydaraliyevasevdo26@gmail.com>
2026-09-20 16:47:17 +0500

    feat: add star, favorite, arrow and upload icons

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -5,6 +5,11 @@ export enum IconName {
   Chat = 'chat',
   RssFeed = 'rss-feed',
   Code = 'code',
+  Star = 'star',
+  Favorite = 'favorite',
+  ArrowBack = 'arrow-back',
+  ArrowForward = 'arrow-forward',
+  Upload = 'upload',
 }
 
 const ICON_PATHS: Record<IconName, string> = {
@@ -16,6 +21,13 @@ const ICON_PATHS: Record<IconName, string> = {
     'M6.18 15.64a2.18 2.18 0 1 0 0 4.36 2.18 2.18 0 0 0 0-4.36zM4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z',
   [IconName.Code]:
     'M9.4 16.6 4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0 4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z',
+  [IconName.Star]:
+    'M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z',
+  [IconName.Favorite]:
+    'M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z',
+  [IconName.ArrowBack]: 'M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z',
+  [IconName.ArrowForward]: 'm12 4-1.41 1.41L16.17 11H4v2h12.17l-5.59 5.59L12 20l8-8z',
+  [IconName.Upload]: 'M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z',
 };
 
 export function iconHtml(name: IconName, className = ''): string {
