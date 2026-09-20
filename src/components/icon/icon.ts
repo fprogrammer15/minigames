14ebd8f8f974f9a94c95ff6d2ca38d19b3318193
dev <haydaraliyevasevdo26@gmail.com>
2026-09-20 18:15:15 +0500

    feat: add auth dialog tokens and form icons

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -10,6 +10,11 @@ export enum IconName {
   ArrowBack = 'arrow-back',
   ArrowForward = 'arrow-forward',
   Upload = 'upload',
+  Mail = 'mail',
+  Lock = 'lock',
+  Person = 'person',
+  Visibility = 'visibility',
+  VisibilityOff = 'visibility-off',
 }
 
 const ICON_PATHS: Record<IconName, string> = {
@@ -28,6 +33,16 @@ const ICON_PATHS: Record<IconName, string> = {
   [IconName.ArrowBack]: 'M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z',
   [IconName.ArrowForward]: 'm12 4-1.41 1.41L16.17 11H4v2h12.17l-5.59 5.59L12 20l8-8z',
   [IconName.Upload]: 'M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z',
+  [IconName.Mail]:
+    'M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z',
+  [IconName.Lock]:
+    'M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1s3.1 1.39 3.1 3.1v2z',
+  [IconName.Person]:
+    'M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z',
+  [IconName.Visibility]:
+    'M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z',
+  [IconName.VisibilityOff]:
+    'M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z',
 };
 
 export function iconHtml(name: IconName, className = ''): string {
